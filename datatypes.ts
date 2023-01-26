56821ac8c9203b559769db731922442b3f6fb47a
gabrielgatbonton <gabriel.gatbonton3262@gmail.com>
2023-01-26 23:07:29 +0800

    Arrays 1

--- datatypes.ts
+++ datatypes.ts
@@ -14,3 +14,21 @@ age = 25.5;
 let dob = "25";
 
 let result = parseInt(dob);
+
+let isValid : boolean = false;
+
+console.log(isValid);
+
+let empList : string[];
+
+empList = ["Gabriel","Gabriel","Gabriel",];
+
+let numList : Array<number>;
+
+numList = [1,2,3,4];
+
+let results = numList.filter((num) => num > 2);
+let num = numList.find((num) => num === 2);
+
+console.log(results);
+console.log(num);
