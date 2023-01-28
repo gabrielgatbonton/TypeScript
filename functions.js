62a4f0f367ea86911dcd7f57b099021f438f9dda
gabrielgatbonton <gabriel.gatbonton3262@gmail.com>
2023-01-28 10:34:55 +0800

    Functions

---
+++ functions.js
@@ -0,0 +1,12 @@
+"use strict";
+function add(num1, num2) {
+    return num1 + num2;
+}
+;
+console.log(add(1, 6));
+const sub = (num1, num2) => num1 - num2;
+console.log(sub(1, 6));
+const multi = function (num1, num2) {
+    return num1 * num2;
+};
+console.log(multi(5, 6));
---
+++ functions.ts
@@ -0,0 +1,15 @@
+function add(num1: number, num2: number): number {
+    return num1 + num2;
+};
+
+console.log(add(1, 6));
+
+const sub = (num1: number, num2: number): number => num1 - num2;
+
+console.log(sub(1, 6));
+
+const multi = function (num1: number, num2: number): number {
+    return num1 * num2
+};
+
+console.log(multi(5, 6));
