7caf14130449290d0f42382965190d1801cbae2e
gabrielgatbonton <gabriel.gatbonton3262@gmail.com>
2023-01-28 11:19:00 +0800

    Function: Optional, Required, Rest

--- functions.js
+++ functions.js
@@ -1,12 +1,20 @@
 "use strict";
-function add(num1, num2) {
-    return num1 + num2;
+function add(num1, num2, num3) {
+    return num3 ? num1 + num2 + num3 : num1 + num2;
 }
 ;
 console.log(add(1, 6));
-const sub = (num1, num2) => num1 - num2;
+console.log(add(1, 6, 3));
+const sub = (num1, num2, num3 = 10) => num1 - num2 - num3;
 console.log(sub(1, 6));
+console.log(sub(1, 6, 5));
 const multi = function (num1, num2) {
     return num1 * num2;
 };
 console.log(multi(5, 6));
+function add2(num1, num2, ...num3) {
+    return num1 + num2 + num3.reduce((a, b) => a + b, 0);
+}
+let numbers = [1, 2, 3, 4, 5];
+console.log(add2(1, 4, ...numbers));
+console.log(add2(1, 4, ...[1, 2, 3, 4, 5]));
--- functions.ts
+++ functions.ts
@@ -1,15 +1,27 @@
-function add(num1: number, num2: number): number {
-    return num1 + num2;
+function add(num1: number, num2: number, num3?: number): number {
+    return num3 ? num1 + num2 + num3 : num1 + num2;
 };
 
 console.log(add(1, 6));
 
-const sub = (num1: number, num2: number): number => num1 - num2;
+console.log(add(1, 6, 3));
+
+const sub = (num1: number, num2: number, num3: number = 10): number => num1 - num2 - num3;
 
 console.log(sub(1, 6));
+
+console.log(sub(1, 6, 5));
 
 const multi = function (num1: number, num2: number): number {
     return num1 * num2
 };
 
 console.log(multi(5, 6));
+
+function add2(num1: number, num2: number, ...num3: number[]): number {
+    return num1 + num2 + num3.reduce((a, b) => a + b, 0);
+}
+
+let numbers = [1,2,3,4,5];
+console.log(add2(1,4,...numbers));
+console.log(add2(1,4,...[1,2,3,4,5]));
